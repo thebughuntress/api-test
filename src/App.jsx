fafab19312ff45fc18fff2765489db4a8663ca7c
Frey, Antonia (EXTERN: Devoteam) <antonia.frey@devoteam.com>
2025-01-11 10:56:03 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import "./App.css";
+
+import { getPosts, getPostById } from "./api";
+
+function App() {
+  const [post, setPost] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchPost = async () => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const number = Math.floor(Math.random() * 100) + 1;
+      const post = await getPostById(number);
+      setPost(post);
+    } catch (error) {
+      setError("Failed to fetch post");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>API Test</h1>
+        <button onClick={fetchPost} disabled={loading}>
+          {loading ? "Loading..." : "Fetch Data"}
+        </button>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        {!error && post && (
+          <div>
+            <h2>Data:</h2>
+            <pre
+              style={{
+                whiteSpace: "pre-wrap",
+                overflowY: "hidden",
+                overflowX: "auto",
+                maxWidth: "100%",
+                padding: "10px",
+                backgroundColor: "#f4f4f4",
+                borderRadius: "5px",
+                marginTop: "10px",
+              }}
+            >
+              {JSON.stringify(post, null, 2)}
+            </pre>
+          </div>
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
